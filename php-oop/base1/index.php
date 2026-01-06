31f4a8b4830a2c52664fc80f495027b2db8b8a23
TG0Ds <168189730+TG0Ds@users.noreply.github.com>
2026-01-06 13:04:09 +0700

    add pengalaman terbaik

---
+++ php-oop/base1/index.php
@@ -0,0 +1,17 @@
+<?php 
+$connection = mysqli_connect(
+    "localhost", "root", "", "kibar_Xii_pplg1");
+    echo var_dump($connection);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Belajar PHP</h1>
+</body>
+</html>
---
+++ php-oop/base2/index.php
@@ -0,0 +1,18 @@
+<?php
+$connect = mysqli_connect(
+    "localhost", "root", "", "kibar_Xii_pplg1");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        
+    </form>
+</body>
+</html>
